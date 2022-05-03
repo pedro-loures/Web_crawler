7af49ec853337d394953455c403727bd23cabbb1
pedro-loures <PLoures2@hotmail.com>
2022-05-03 09:39:05 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Web_crawler
+ Informantion Retrieval work aimed to to implement a crawler capable of fetching a mid-sized corpus of webpages in a short time frame while respecting the politeness constraints defined by each crawled website
---
+++ main.py
@@ -0,0 +1,18 @@
+import sys 
+import beautifulsoup4
+import cachetools
+import certifi
+import charset
+import idna
+import python
+import reppy
+import requests
+import six
+import soupsieve
+import url
+import urllib3
+import warcio
+
+if __name__ == "__main__":
+    print(sys.argv)
+
