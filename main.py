54b7ef28c8cb161476e4665d09682da2415b04f8
pedro-loures <PLoures2@hotmail.com>
2022-05-11 15:20:30 -0300

    update (especificidade continua baixa)
    conferir resultado do tratamento em "dados_tratados_1_bin_" que são apeans as colunas com dois desvios padrão

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-#1 TODO Get arguments from command line
+#0 OK assert usage
+#1 OK Get arguments from command line
 #2 TODO Implement good practices
 #3 TODO Reach seed
 #4 TODO Read and robots.txt
@@ -6,21 +7,31 @@
 #6 TODO Create heap from seed anchor text
 #7 TODO implement multithreading
 
+
+# CONVENTIONS:
+#   fun() -> Uses action
+#   var   -> Uses states
+
+
+PATH_TO_CORPUS = "C:\\Users\\PLour\\OneDrive - Universidade Federal de Minas Gerais\\01_Estudos\\Faculdade\\RI\\Crawler\\Corpus"
+
 # external modules
 import sys 
+import requests
 
 # internal modules
-import utils
+import utils as ut
 
 if __name__ == "__main__":
     argv = sys.argv
 
-    # Assert Usage
-    assert len(argv) > 5, "USAGE: python3 main.py -s <SEEDS> -n <LIMIT> [-d]"
-    
-    # Get arguments
-    print(get_opt(argv))
-    
-    # 
+    # 0, 1 Assert Usage and get options
+    seed_url, page_limit, is_debug = ut.get_opt(argv)
+ 
+    # 3
+    r = requests.get('http://portalms.saude.gov.br/', auth=('user', 'pass'))
+    print(r.status_code)
+
+     
 
     
--- utils.py
+++ utils.py
@@ -1,16 +1,45 @@
 import sys
 
+def _assert_usage_len(argv):
+    assert len(argv) > 5, "USAGE: python3 main.py -s <SEEDS> -n <LIMIT> [-d]"
+
+def _assert_usage_arguments(argument):
+    if len(argument) == 1:
+        assert argument[0] == '-d', "USAGE: python3 main.py -s <SEEDS> -n <LIMIT> [-d]"
+    else:
+        assert argument[0] in ['-s', '-n'], "USAGE: python3 main.py -s <SEEDS> -n <LIMIT> [-d]"
+
 def get_opt(argv):
-    opt = []
-    i = 0
-    while i < len(argv[:-1]):
+    # Return
+    seed_url = ''
+    page_limit = ''
+    is_debug = False
+    
+    # Assert correct number of argument
+    _assert_usage_len(argv)
+    
 
-        i+=1
-        opt.append([argv[i]])
-        if argv[i][0] == '-' and i + 1 < len(argv):
-            print(opt[-1])
-            opt[-1].append(argv[i+1])
-            i+=1
+    # separate argument in list of list
+    _options = []
+    _i = 0
+    while _i < len(argv[:-1]):
 
-    return opt
+        _i+=1
+        _options.append([argv[_i]])
+        if argv[_i][0] == '-' and _i + 1 < len(argv):
+            _options[-1].append(argv[_i+1])
+            _i+=1
+
+    # Fill return values and assert correct input argument usage
+    for _option in _options:
+        _assert_usage_arguments(_option)
+        if _option[0] == '-d':
+            is_debug = True 
+        elif _option[0] == '-n':
+            page_limit = _option[1]
+        elif _option[0] == '-s':
+            seed_url = _option[1]
+
+
+    return seed_url, page_limit, is_debug
 
