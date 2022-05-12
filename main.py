426bc86aa61a283fbaa3b2aa817cc68af0e0d5a7
pedro-loures <PLoures2@hotmail.com>
2022-05-12 02:39:19 -0300

    update fetch and queue

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
-#0 OK assert usage
-#1 OK Get arguments from command line
-#2 TODO Implement good practices
-#3 TODO Reach seed
-#4 TODO Read and robots.txt
-#5 TODO Collect seed
-#6 TODO Create heap from seed anchor text
-#7 TODO implement multithreading
+#-0 OK assert usage
+#-1 OK Get arguments from command line
+#-2 Reach seed
+#-3 OK Collect seed
+#-4 TODO Implement good practices
+#-5 TODO Read and respect robots.txt
+#-6 TODO Create heap from seed anchor text
+#-7 TODO parse heap
+#-8 TODO save corpus
+#-9 TODO implement multithreading
+#10 TODO Create Debug mode
+
+
+# 4.1 TODO: implement a default of at least 100ms between consecutive request to the same website
+# 4.2 TODO: abide by the Robots eclusion standart
+
+# 6.1 TODO: implement selection policy (only follow links to html pages)
+# 6.2 TODO: implement profundity limit
+# 6.3 ok: implement normalization and enqueing policy
+# 6.4 TODO: implement revisitation policy
+
+
+
+# 8.1 TODO: save raw HTML content using WARC format (1000 webpages per file)
+# 8.2 TODO: compress with gzip
 
 
 # CONVENTIONS:
@@ -18,20 +35,41 @@ PATH_TO_CORPUS = "C:\\Users\\PLour\\OneDrive - Universidade Federal de Minas Ger
 # external modules
 import sys 
 import requests
+from reppy.robots import Robots # ler robots.txt
+from bs4 import BeautifulSoup # parse html
 
 # internal modules
-import utils as ut
+import utils as ut # funções pessoais
 
-if __name__ == "__main__":
-    argv = sys.argv
+def main(argv):
+    
 
     # 0, 1 Assert Usage and get options
     seed_url, page_limit, is_debug = ut.get_opt(argv)
  
     # 3
-    r = requests.get('http://portalms.saude.gov.br/', auth=('user', 'pass'))
-    print(r.status_code)
+    url = 'http://portalms.saude.gov.br/'
+    page = requests.get(url, auth=('user', 'pass'))
+    
+    #
+    soup = BeautifulSoup(page.content, 'html.parser')
+    print(len(ut.add_url_to_queue(soup, url)))
+    
+    # # This utility uses `requests` to fetch the content
+    # robots = Robots.fetch('http://portalms.saude.gov.br/robots.txt')
+    # robots.allowed('http://portalms.saude.gov.br/', 'my-user-agent')
+
+
+    # # Get the rules for a specific agent
+    # agent = robots.agent('my-user-agent')
+    # agent.allowed('http://portalms.saude.gov.br/')
 
      
 
     
+    
+
+
+if __name__ == "__main__":
+    argv = sys.argv
+    main(argv)
--- utils.py
+++ utils.py
@@ -1,4 +1,27 @@
-import sys
+#####################
+# File: Utils.py
+# Author(s): Pedro Loures Alzamora
+# last update:  11/05/2022
+####################
+
+# external modules
+import sys # read argv
+from reppy.robots import Robots # read robots.txt
+from bs4 import BeautifulSoup # parse html
+
+# Internal modules
+
+
+
+
+# Index
+#   # Internal functions
+#       def _assert_usage_len(argv): # assert argv is of correct len              
+#       def _assert_usage_arguments(argument): # assert argv has the right arguments
+# 
+#   # External functions
+#       def get_opt(argv): # assert usage and return the correct arguments of the argv
+#
 
 def _assert_usage_len(argv):
     assert len(argv) > 5, "USAGE: python3 main.py -s <SEEDS> -n <LIMIT> [-d]"
@@ -9,6 +32,7 @@ def _assert_usage_arguments(argument):
     else:
         assert argument[0] in ['-s', '-n'], "USAGE: python3 main.py -s <SEEDS> -n <LIMIT> [-d]"
 
+# assert usage and return the correct arguments of the argv
 def get_opt(argv):
     # Return
     seed_url = ''
@@ -40,6 +64,46 @@ def get_opt(argv):
         elif _option[0] == '-s':
             seed_url = _option[1]
 
-
     return seed_url, page_limit, is_debug
 
+def _merge_ordered_dicts(dict1, dict2):
+    return
+
+# add urls from a page to queue
+def add_url_to_queue(HTML_soup, HTML_url, url_queue, _url_depth=5):
+    _a_tags = HTML_soup.find_all('a')
+    _new_url_queue = {}
+    for _tag in _a_tags:
+        # url treatment policy
+        _tag_url = _tag.get('href')
+        if not isinstance(_tag_url, str):
+            continue
+        if _tag_url[0] == '#':
+            continue
+        if _tag_url[0] == '/': 
+            _tag_url = HTML_url + _tag_url[1:]   
+        if _tag_url[:4] == 'http':
+            _tag_url = _tag_url.split('/', maxsplit=2)[2]
+        if _tag_url[:4] == 'www.':
+            _tag_url = _tag_url[4:]
+        
+        # profundity limit
+        _url_path = _tag_url.split('/')
+        if len(_url_path) > _url_depth:
+#            print(_tag_url)
+#            print(_url_path)
+            continue
+
+
+#        print(_tag_url)
+        # Enquewing policy
+        _new_url_queue[_tag_url] = _tag_url
+    return _new_url_queue
+
+def check_robots(url):
+    return
+
+# save html soup produced by beatiful soup
+# def store_soup(soup, path_to_fridge):
+#     with open('teste.txt', 'w') as f:
+#         f.write(str(r.text))
